4093cebf0050be0509687f63d61d9f1b85134dbd
Rishabh Arya <79662499+rishabh-arya@users.noreply.github.com>
2021-10-02 12:27:29 +0530

    Create Combination_sum_39.cpp

---
+++ Leetcode/Combination_sum_39.cpp
@@ -0,0 +1,31 @@
+// Link to the problem :  https://leetcode.com/problems/combination-sum/
+
+class Solution {
+public:
+    void sum(vector<int>& candidates,int target,vector<vector<int>>& ans,int i,vector<int> &t)
+    {
+        if(i >= candidates.size() || target < 0)
+            return;
+        
+        if(target == 0){
+            ans.push_back(t);
+            return;
+        }
+        t.push_back(candidates[i]);
+       
+        sum(candidates,target-candidates[i],ans,i,t);
+        t.pop_back();
+        sum(candidates,target,ans,i+1,t);
+        return;
+ 
+
+        
+            
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> t;
+        sum(candidates,target,ans,0,t);
+        return ans;
+    }
+};
